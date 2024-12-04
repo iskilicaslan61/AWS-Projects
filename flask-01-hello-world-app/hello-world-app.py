843e3d7e64b70fed71880706aa48c72aed5894a6
Ismail <iskilicaslan61@gmail.com>
2024-12-04 10:54:08 +0100

    first portfolio

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,16 +1,23 @@
-from flask import Flask 
+from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def head():
+     return 'Hello World Mikail'
 
+@app.route('/secondpage')
+def second():
+     return 'This is second page'
 
+@app.route('/third')
+def third():
+     return 'This is third page'
 
-
-
-
-
-
+@app.route('/fourth/<string.id>')
+def fourth(id):
+     return f'Id of this page is {id}'
 
 if __name__ == '__main__':
-
-     app.run(debug=True, port=3000)
-     # app.run(host= '0.0.0.0', port=8081)
+    
+     app.run(debug=True)
+     #app.run(host= '0.0.0.0', port=80)
